502410dc0c2dc2503ff85b29b824d581e13c06d8
Marcus Neves <marcus-neves@outlook.com>
2023-07-27 21:28:37 -0300

    feature: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <GeistProvider>
       <CssBaseline />
         <Component {...pageProps} />
+        <Analytics />
     </GeistProvider>
     </ChakraProvider>
   )
